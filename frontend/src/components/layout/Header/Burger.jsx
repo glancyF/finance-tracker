f5b3340ec0e1003d836f6d27dfbd7aeb9dfe241e
glancy <valentindesel@gmail.com>
2025-09-05 17:08:32 +0200

    Update assets

--- frontend/src/components/layout/Header/Burger.jsx
+++
@@ -1,9 +0,0 @@
-export default function Burger ({open}){
-    return (
-        <div className="space-y-1.5">
-            <span className={`block h-0.5 w-6 bg-current transition ${open ? "translate-y-2 rotate-45" : ""}`} />
-            <span className={`block h-0.5 w-6 bg-current transition ${open ? "opacity-0" : ""}`} />
-            <span className={`block h-0.5 w-6 bg-current transition ${open ? "-translate-y-2 -rotate-45" : ""}`} />
-        </div>
-    )
-}
