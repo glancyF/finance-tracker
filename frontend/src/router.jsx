b7b1ed3331364a63a8291e8bbd529eb019dad6a7
glancy <valentindesel@gmail.com>
2025-09-07 13:06:34 +0200

    Update routes and set correct redirect on main page

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,14 +1,20 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./Pages/Home.jsx";
-import Login from "./Pages/Login.jsx"
-import Register from "./Pages/Register.jsx"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./features/auth/AuthContext.jsx";
+import Home from "./pages/Home.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 function Router (){
     return (
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-        </Routes>
+        <AuthProvider>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </AuthProvider>
     )
 }
 
