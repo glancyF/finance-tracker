ef00edbbb4034cbe047fd9ca66c8702207778ade
glancy <valentindesel@gmail.com>
2025-09-09 15:34:46 +0200

    Implement server logic for profile settings update and password change

---
+++ backend/app/Http/Requests/Profile/UpdateProfileRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Profile;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string','min:3','max:16','regex:/^\p{L}+$/u'],
+            'email' => ['required', 'email','min:1','max:254', Rule::unique('users','email')->ignore($this->user()->id)],
+        ];
+    }
+}
